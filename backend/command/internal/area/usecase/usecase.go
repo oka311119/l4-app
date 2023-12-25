9839724372ba24c6d8574525300bc764fa8e4a86
oka311119 <katoko.318@gmail.com>
2023-12-25 23:57:59 +0900

    temp

--- backend/command/internal/area/usecase/usecase.go
+++ backend/command/internal/area/usecase/usecase.go
@@ -2,27 +2,35 @@ package usecase
 
 import (
 	"context"
+	"time"
 
 	"github.com/oka311119/l4-app/backend/command/internal/area"
 	"github.com/oka311119/l4-app/backend/command/internal/domain/entity"
+	"github.com/oka311119/l4-app/backend/command/internal/helpers/uuidgen"
 )
 
 type AreaUseCase struct {
 	areaRepo area.Repository
+	uuidgen uuidgen.UUIDGenerator
 }
 
 func NewAreaUseCase(
 	areaRepo area.Repository,
+	uuidgen uuidgen.UUIDGenerator,
 ) *AreaUseCase {
 	return &AreaUseCase{
 		areaRepo: areaRepo,
+		uuidgen: uuidgen,
 	}
 }
 
 func (a *AreaUseCase) CreateDefaultArea(ctx context.Context) error {
-	area := &entity.Area{
-		ID
-	}
+	area := entity.NewArea(
+		a.uuidgen.V4(),
+		a.uuidgen.V4(),	//userid
+		"default",
+		time.Now(),
+	) 
 
 	return a.areaRepo.CreateArea(ctx, area)
 }
--- backend/command/internal/domain/entity/area.go
+++ backend/command/internal/domain/entity/area.go
@@ -8,3 +8,17 @@ type Area struct {
 	Name string
 	CreatedAt time.Time
 }
+
+func NewArea(
+	id string,
+	userId string,
+	name string,
+	createdAt time.Time,
+) *Area {
+	return &Area{
+		ID: id,
+		UserID: userId,
+		Name: name,
+		CreatedAt: createdAt,
+	}
+}
