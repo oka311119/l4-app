464cbb627e5b36bfefe80099795ea6f158e8bc2c
oka311119 <katoko.318@gmail.com>
2024-01-07 23:10:55 +0900

    temp

--- backend/command/internal/area/usecase/usecase_test.go
+++ backend/command/internal/area/usecase/usecase_test.go
@@ -1,38 +1,38 @@
 package usecase
 
 import (
-    "context"
-    "testing"
+	"context"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/oka311119/l4-app/backend/command/internal/area/repository/mock"
 	"github.com/oka311119/l4-app/backend/command/internal/domain/entity"
 	"github.com/oka311119/l4-app/backend/command/internal/helpers/uuidgen"
 )
 
 func TestAreaFlow(t *testing.T) {
-    repo := new(mock.AreaStorageMock)
-    uc := NewAreaUseCase(repo, &uuidgen.MockUUID{})
+	repo := new(mock.AreaStorageMock)
+	uc := NewAreaUseCase(repo, &uuidgen.MockUUID{})
 
-    var (
-        areaID = uc.uuidgen.V4()
-        userID = uc.uuidgen.V4()
-        name = "area-name"
-        area = entity.NewArea(
-            areaID,
-            userID,
-            "default",
-        )
-        ctx = context.Background()
-    )
+	var (
+		userID = uc.uuidgen.V4()
+		name   = "area"
+		area   = entity.NewArea(
+			userID,
+			name,
+			"default",
+		)
+		ctx = context.Background()
+	)
 
-    // Create Default Area
-    repo.On("CreateDefaultArea", area).Return(nil)
-    err := uc.CreateDefaultArea(ctx, userID)
-    assert.NoError(t, err)
+	// Create Default Area
+	repo.On("CreateDefaultArea", area).Return(nil)
+	err := uc.CreateDefaultArea(ctx, userID)
+	assert.NoError(t, err)
 
-    // Create Area
-    repo.On("CreateArea", area).Return(nil)
-    err := uc.CreateArea(ctx, userID, name)
-    assert.NoError(t, err)
+	// Create Area
+	repo.On("CreateArea", area).Return(nil)
+	err = uc.CreateArea(ctx, userID, name)
+	assert.NoError(t, err)
 }
