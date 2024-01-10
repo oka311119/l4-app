a718aabf1e655d83fcc3a67694cd332a1af7457b
oka311119 <katoko.318@gmail.com>
2024-01-10 22:35:04 +0900

    temp

--- backend/command/internal/area/repository/localstorage/area.go
+++ backend/command/internal/area/repository/localstorage/area.go
@@ -1,1 +1,31 @@
 package localstorage
+
+import (
+	"context"
+	"sync"
+
+	"github.com/oka311119/l4-app/backend/command/internal/area"
+	"github.com/oka311119/l4-app/backend/command/internal/domain/entity"
+)
+
+
+type AreaLocalStorage struct {
+	areas map[string]*entity.Area
+	mutex *sync.Mutex
+}
+
+func NewAreaLocalStorage() *AreaLocalStorage {
+	return &AreaLocalStorage{
+		areas: make(map[string]*entity.Area),
+		mutex: new(sync.Mutex),
+	}
+}
+
+func (s *AreaLocalStorage) CreateArea(ctx context.Context, area *entity.Area) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.areas[area.ID] = area
+	return nil
+}
+
--- backend/command/internal/area/repository/localstorage/area_test.go
+++ backend/command/internal/area/repository/localstorage/area_test.go
@@ -1,1 +1,24 @@
 package localstorage
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/oka311119/l4-app/backend/command/internal/domain/entity"
+)
+
+func TestCreateArea(t *testing.T) {
+	s := NewAreaLocalStorage()
+
+	id1 := "id"
+	area := &entity.Area{
+		ID: id1,
+		UserID: "id",
+		Name: "area-name",
+    }
+
+    err := s.CreateArea(context.Background(), area)
+	assert.NoError(t, err)
+}
